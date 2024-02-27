31992de378b4790db5131a2fc4f57732bddedea4
acarbf01 <acarbf01@estudiantes.unileon.es>
2024-02-27 12:09:20 +0100

    variables globales y solicitudes al usuario

--- Decodificacion.py
+++ Decodificacion.py
@@ -7,13 +7,21 @@ palabra = ""
 
 l_palabra = 0
 
+#Relleno la lista de caracteres
+abecedario = [chr(i) for i in range (101,133)]
+abecedario.append(".")
+abecedario.append(" ")
+
+#Relleno la lista de probabilidades
+prob = [j + 1 for j in range (len(abecedario))]
+
 #Defino funciones
 
 #Funcion que comprueba el numero actual del mensaje codfificado y añade la letra correspondiente al mensaje. Devuelve el intervalo.
 def devuelveCarac(n_codif):
 
     #Bucle que comprueba a que letra pertenece el numero
-    for i in range(0,len(abecedario)-1): 
+    for i in range(0,len(abecedario)): 
 
         #Comprueba si la probabilidad es mayor y, por lo tanto, pertenece al intervalo
         if prob[i] > n_codif:
@@ -54,7 +62,14 @@ if func == 'd' | func == 'D':
 
 #Opcion codificacion
 elif func == 'c' | func == 'C':
-    print()
+    
+    #Bucle que solicita al usuario la longitud. Comprueba que la longitud es valida y si no lo es pide el dato hasta que sea valida
+    while palabra.isAlpha() | palabra.isspace() | palabra.find(".") in range(0,len(palabra)):
+        palabra = input("Introduce la palabra a codificar: ")
+        if l_palabra <= 0:
+            print("La palabra solo debe contener letras, '.' o ' '")
+    
+    #
 
 else: 
     print("Por favor, introduce una opción válida.")
