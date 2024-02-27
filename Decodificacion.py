7f126c2ff67891b547e685616d7ac4c49993e81f
acarbf01 <acarbf01@estudiantes.unileon.es>
2024-02-27 11:39:21 +0100

    solicitud de datos decod.

--- Decodificacion.py
+++ Decodificacion.py
@@ -5,6 +5,7 @@
 #Declaración de variables
 palabra = ""
 
+l_palabra = 0
 
 #Defino funciones
 
@@ -30,15 +31,30 @@ def actualizaNum(n_codif, prob1, prob2):
 
 
 #main
-
+func = input("Introduce la función a realizar:")
 #Opcion decodificacion
 if func == 'd' | func == 'D':
-    
+
+    #Bucle que solicita al usuario la longitud. Comprueba que la longitud es valida y si no lo es pide el dato hasta que sea valida
+    while l_palabra <= 0:
+        l_palabra = int(input("Introduce la longitud de la palabra: "))
+        if l_palabra <= 0:
+            print("La longitud de la palabra debe de ser un número natural.")
+
+    #Bucle que solicita al usuario el numero a decodificar. Comprueba que el valor es valido y si no lo solicita hasta que sea valido
+    while n_codif <= 0:
+        n_codif = int (input("Introduce el numero de la palabra a codifciar: "))
+        if n_codif <= 0:
+            print("El numero a decodificar debe de ser un número racional positivo.")
+
     #Bucle que realizara la codificacion de la palabra dependiendo de la longitud dada de esta.
-    for i in range (0, longitud_msj):
+    for i in range (0, l_palabra):
         n_codif = actualizaNum(n_codif, devuelveCarac(n_codif)[0], devuelveCarac(n_codif)[1])
     print(palabra)
 
 #Opcion codificacion
 elif func == 'c' | func == 'C':
     print()
+
+else: 
+    print("Por favor, introduce una opción válida.")
